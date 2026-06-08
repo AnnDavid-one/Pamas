2819c075e5f73e77b5758f7ba11622591b5ee0b1
DavidHartUlea <duchenna59@gmail.com>
2026-06-08 05:21:11 +0100

    fixed links

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -6,20 +6,29 @@ import { GoogleLogo } from "@/assets/images"; // Import Google logo from assets
 import Link from "next/link";
 
 export default function SignupPage() {
+
+  const handleGoogleLogin = () => {
+    const apiBase = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000";
+    window.location.href = `${apiBase}/auth/google`;
+  };
+
+
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-foreground p-5">
       <div className="flex flex-col w-full max-w-sm h-[60vh] bg-card absolute rounded-xl shadow-lg p-6 justify-between">
         <h2 className="text-3xl font-bold text-white mb-6">Sign In to Pamas Ai</h2>
 
         {/* Redirect to backend Google OAuth */}
-        <Link href={process.env.NEXT_PUBLIC_API_BASE_URL + '/auth/google'}>
+        {/* <Link href={process.env.NEXT_PUBLIC_API_BASE_URL + '/auth/google'}> */}
           <Button
             className="w-full bg-primary text-xl hover:bg-primary/80 text-white font-bold py-5 px-4 rounded-xl mt-4"
-          >
+         onClick={handleGoogleLogin}
+         >
             <Image src={GoogleLogo} alt="Google Logo" width={20} height={20} />
             Continue with Google
           </Button>
-        </Link>
+        {/* </Link> */}
 
         <p className="text-sm text-gray-400 mt-4">
           To access your account, we’ll send you a secure sign-in link via email.
